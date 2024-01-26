eee7b8e8f932e244a3f8a311a87b430bbad73efa
shahwaz746 <shahwazakhter746@gmail.com>
2024-01-26 16:00:46 +0530

    commit again

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.querySelector("#about").addEventListener("click",function(){
 
     alert("the progress on the peak")
 })
+document.querySelector("#contect").addEventListener("click",function(){
+
+
+    alert("the progress on the peak")
+})
+document.querySelector("#pp").addEventListener("click",function(){
+
+
+    alert("the progress on the peak")
+})
+
+var userConfirmation = confirm("hii ' i am shahwaz akhter developer of this website");
+
