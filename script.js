97366b0cf18c260c5ead220e9e9ce0311d39e193
shahwaz746 <shahwazakhter746@gmail.com>
2024-01-26 19:51:56 +0530

    commit again

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
-document.querySelector("#pp").addEventListener("click",function(){
-
-
-    alert("the progress on the peak")
-})
 
 var userConfirmation = confirm("hii ' i am shahwaz akhter developer of this website");
 
