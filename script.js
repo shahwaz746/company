fc6a53e9cf4a3d0b14c2de5a35bf52ae027af802
shahwaz746 <shahwazakhter746@gmail.com>
2024-01-26 19:00:35 +0530

    commit again

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-document.querySelector("#about").addEventListener("click",function(){
 
-
-    alert("the progress on the peak")
-})
-document.querySelector("#contect").addEventListener("click",function(){
-
-
-    alert("the progress on the peak")
-})
 document.querySelector("#pp").addEventListener("click",function(){
 
 
